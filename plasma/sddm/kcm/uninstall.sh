55ff6f34eb9fb55c056544b61c87038d64c953d2
catpswin56 <catpswin5@proton.me>
2026-03-12 17:41:56 -0300

    Old SDDM KCM: improve uninstall.sh See previous commit for more information

--- plasma/sddm/kcm/uninstall.sh
+++ plasma/sddm/kcm/uninstall.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-cd build/
-
 BUILD_TOOL=make
+SU_CMD=sudo
+if [[ -z "$(command -v $SU_CMD)" ]]; then
+    SU_CMD=doas
+    if [[ -z "$(command -v $SU_CMD)" ]]; then
+        echo "Neither sudo or doas were detected on the system."
+        exit
+    fi
+fi
+
+# install first
+cmake -B build -DCMAKE_INSTALL_PREFIX=/usr .
+cmake --build build
+$SU_CMD cmake --install build
+
+# uninstall later
+cd build/
 if [ -f "build.ninja" ]; then
     BUILD_TOOL=ninja
 fi
